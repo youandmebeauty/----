775c6a9f3b9e65a17bdfef181af0f2a0c7083bfd
youandmebeauty <youmebeauty282@gmail.com>
2025-12-11 10:36:04 +0100

    feat: Update footer social media links to point to the correct Facebook and TikTok pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -57,7 +57,7 @@ export function Footer() {
 </a>
 
 <a
-  href="#"
+  href="https://www.facebook.com/people/YOUME-Beauty/61578933269826/"
   target="_blank"
   rel="noopener noreferrer"
   className={`w-10 h-10 rounded-full bg-primary/10 flex items-center justify-center ${
@@ -69,7 +69,7 @@ export function Footer() {
 </a>
 
 <a
-  href="#"
+  href="https://www.tiktok.com/@youmebeauty"
   target="_blank"
   rel="noopener noreferrer"
   className={`w-10 h-10 rounded-full bg-primary/10 flex items-center justify-center ${
