70b8dccd13ee17d56288009c1aee35f073a45826
youandmebeauty <youmebeauty282@gmail.com>
2026-04-17 18:25:29 +0200

    feat: update Content Security Policy to include additional sources for script and frame

--- middleware.ts
+++ middleware.ts
@@ -7,12 +7,12 @@ export function middleware(request: NextRequest) {
   // Enforce a baseline Content Security Policy to mitigate XSS and injection.
   const csp = [
     "default-src 'self'",
-    `script-src 'self' https://www.googletagmanager.com/gtag/ https://connect.facebook.net https://www.gstatic.com https://apis.google.com https://ajax.googleapis.com https://cdn.jsdelivr.net 'wasm-unsafe-eval' 'sha256-J9cZHZf5nVZbsm7Pqxc8RsURv1AIXkMgbhfrZvoOs/A=' 'nonce-${nonce}'`,
+    `script-src 'self' https://www.googletagmanager.com/gtag/ https://connect.facebook.net https://www.gstatic.com https://apis.google.com https://ajax.googleapis.com https://cdn.jsdelivr.net https://widget.cloudinary.com https://upload-widget.cloudinary.com 'wasm-unsafe-eval' 'sha256-J9cZHZf5nVZbsm7Pqxc8RsURv1AIXkMgbhfrZvoOs/A=' 'nonce-${nonce}'`,
     "style-src 'self' 'unsafe-inline'",
     "img-src 'self' data: https: https://firebasestorage.googleapis.com https://lh3.googleusercontent.com",
     "font-src 'self' https://fonts.gstatic.com",
-    "connect-src 'self' https://ajax.googleapis.com/ https://connect.facebook.net https://www.google-analytics.com https://region1.google-analytics.com https://www.gstatic.com https://www.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://firestore.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebasestorage.googleapis.com https://cdn.jsdelivr.net https://mpc-prod-25-s6uit34pua-wl.a.run.app https://demo-1.conversionsapigateway.com",
-    "frame-src 'self' https://www.google.com/ https://www.facebook.com",
+    "connect-src 'self' https://ajax.googleapis.com/ https://connect.facebook.net https://www.google-analytics.com https://region1.google-analytics.com https://www.gstatic.com https://www.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://firestore.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebasestorage.googleapis.com https://cdn.jsdelivr.net https://api.cloudinary.com https://res.cloudinary.com https://api.bigdatacloud.net https://api.open-meteo.com https://mpc-prod-25-s6uit34pua-wl.a.run.app https://demo-1.conversionsapigateway.com",
+    "frame-src 'self' https://www.google.com/ https://www.facebook.com https://widget.cloudinary.com https://upload-widget.cloudinary.com",
     "worker-src 'self' blob:",
     "object-src 'none'",
     "base-uri 'self'",
